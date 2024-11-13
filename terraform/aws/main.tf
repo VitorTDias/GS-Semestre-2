8c7ba0aa3dc332d6be8d1262839f65b00ddbc54a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-13 20:42:54 -0300

    teste commit4.1

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -5,7 +5,7 @@ module "rede" {
 module "compute" {
  source = "./modules/compute"
  ec2_ami = "ami-0f409bae3775dc8e5"
- vpc_id = "${module.rede.vpc10_id}"
- subnet_az1a_id = "${module.rede.sn_vpc10_pub1a}"
- subnet_az1b_id = "${module.rede.sn_vpc10_pub1b}"
+ vpc_id = "${module.vpc10_id}"
+ subnet_az1a_id = "${module.sn_vpc10_pub1a}"
+ subnet_az1b_id = "${module.sn_vpc10_pub1b}"
 }
